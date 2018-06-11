3423cbea235c58e500a35c53395530c4366c9659
Saurav <sp.gharti@gmail.com>
2018-06-11 07:30:49 +0000

    add package django-allauth

--- home/views.py
+++ home/views.py
@@ -8,6 +8,7 @@ from home.models import CustomText, HomePage
 def home(request):
     packages = [
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
+	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
